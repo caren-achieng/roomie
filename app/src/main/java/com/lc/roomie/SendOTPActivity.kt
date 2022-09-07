7cc742ac0faedf4e959e84645243009772174350
Leo-Bikuri <leo.bikuri@strathmore.edu>
2022-09-08 00:25:20 +0300

    OTP page fixes

--- app/src/main/java/com/lc/roomie/SendOTPActivity.kt
+++ app/src/main/java/com/lc/roomie/SendOTPActivity.kt
@@ -1,11 +1,20 @@
 package com.lc.roomie
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+
 
 class SendOTPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_send_otp)
     }
 }
