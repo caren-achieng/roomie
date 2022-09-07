717a49228574dff9d2db1c10cf38cac5c30e5962
Leo-Bikuri <leo.bikuri@strathmore.edu>
2022-09-07 14:11:05 +0300

    Trial

--- app/src/main/java/com/lc/roomie/SplashScreenActivity.kt
+++ app/src/main/java/com/lc/roomie/SplashScreenActivity.kt
@@ -1,18 +1,29 @@
 package com.lc.roomie
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.animation.AnimationUtils
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+
         val animation = AnimationUtils.loadAnimation( this, R.anim.logoanimation)
 
-        topTextView.startAnimation(animation)
+        logo.startAnimation(animation)
+
+        val splashScreenTimeout=4000
+        val homeIntent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(homeIntent)
+            finish()
+        }, splashScreenTimeout.toLong())
     }
 }
