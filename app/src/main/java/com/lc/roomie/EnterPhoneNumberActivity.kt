87204b55cbf1a434dcd0674c1a697c036435d26d
caren-achieng <caren.achieng@strathmore.edu>
2022-09-07 21:37:03 +0300

    event listener on create account button

---
+++ app/src/main/java/com/lc/roomie/EnterPhoneNumberActivity.kt
@@ -0,0 +1,11 @@
+package com.lc.roomie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterPhoneNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_phone)
+    }
+}
--- app/src/main/java/com/lc/roomie/LoginActivity.kt
+++ app/src/main/java/com/lc/roomie/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.lc.roomie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        CreateAccountBtn.setOnClickListener{
+            val intent = Intent(this, EnterPhoneNumberActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
