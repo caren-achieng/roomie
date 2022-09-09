e1e9b6d6925f5c6c13a47e6c4e66683cbd82f1eb
caren-achieng <caren.achieng@strathmore.edu>
2022-09-09 22:55:20 +0300

    verify otp view done

--- app/src/main/java/com/lc/roomie/SendOTPActivity.kt
+++ app/src/main/java/com/lc/roomie/SendOTPActivity.kt
@@ -1,14 +1,19 @@
 package com.lc.roomie
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_send_otp.*
+import kotlinx.android.synthetic.main.activity_verify_otp.*
 
 
 class SendOTPActivity : AppCompatActivity() {
@@ -16,5 +21,10 @@ class SendOTPActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_send_otp)
+
+        SendOtp.setOnClickListener{
+            val intent = Intent(this, VerifyOTPActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
