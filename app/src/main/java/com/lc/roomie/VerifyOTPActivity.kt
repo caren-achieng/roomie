0a6c633f63fe8382b2c4734bae066ec809c5dbf2
caren-achieng <caren.achieng@strathmore.edu>
2022-09-12 20:15:54 +0300

    verify email view done. not responsive if email is longer than input line

--- app/src/main/java/com/lc/roomie/VerifyOTPActivity.kt
+++ app/src/main/java/com/lc/roomie/VerifyOTPActivity.kt
@@ -1,11 +1,20 @@
 package com.lc.roomie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_send_otp.*
+import kotlinx.android.synthetic.main.activity_verify_email.*
+import kotlinx.android.synthetic.main.activity_verify_otp.*
 
 class VerifyOTPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_otp)
+
+        buttonVefiry.setOnClickListener{
+            val intent = Intent(this, VerifyEmailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
