b33f39dcb1cc6fb5b40ddd3e7664b1db86b40352
caren-achieng <caren.achieng@strathmore.edu>
2022-09-15 19:05:51 +0300

    house rules

--- app/src/main/java/com/lc/roomie/VerifyEmailActivity.kt
+++ app/src/main/java/com/lc/roomie/VerifyEmailActivity.kt
@@ -1,11 +1,19 @@
 package com.lc.roomie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_verify_email.*
+import kotlinx.android.synthetic.main.activity_verify_otp.*
 
 class VerifyEmailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_email)
+
+        VerifyEmail.setOnClickListener{
+            val intent = Intent(this, HouserulesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
