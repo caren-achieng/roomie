69c8c811f558044114c7a7f757be605f04bc7609
caren-achieng <caren.achieng@strathmore.edu>
2022-09-16 19:35:27 +0300

    added activity

--- app/src/main/java/com/lc/roomie/HouserulesActivity.kt
+++ app/src/main/java/com/lc/roomie/HouserulesActivity.kt
@@ -1,11 +1,19 @@
 package com.lc.roomie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_houserules.*
+import kotlinx.android.synthetic.main.activity_verify_email.*
 
 class HouserulesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_houserules)
+
+        Agree.setOnClickListener{
+            val intent = Intent(this, CompleteProfileActivity::class.java)
+            startActivity(intent)
+    }
     }
 }
