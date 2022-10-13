92a247e73f320a5f5d4e5181426449e606ad13ec
Leo-Bikuri <leo.bikuri@strathmore.edu>
2022-10-13 23:41:17 +0300

    Chips �

--- app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
+++ app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
@@ -30,22 +30,23 @@ class CompleteProfileActivity : AppCompatActivity() {
 
         birthday.calendarViewShown = false
 
-        change_profile_picture.setOnClickListener {
-            if (VERSION.SDK_INT >= VERSION_CODES.M){
-                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
 
-                    var permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-
-                    requestPermissions(permissions, PERMISSION_CODE)
-
-                }else{
-
-                }
-            }else{
-
-            }
-        }
-
+//        change_profile_picture.setOnClickListener {
+//            if (VERSION.SDK_INT >= VERSION_CODES.M){
+//                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+//
+//                    var permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+//
+//                    requestPermissions(permissions, PERMISSION_CODE)
+//
+//                }else{
+//
+//                }
+//            }else{
+//
+//            }
+//        }
+//
     }
 
     fun onRadioButtonClicked(view: View) {}
