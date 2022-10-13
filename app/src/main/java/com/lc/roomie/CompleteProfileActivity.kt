92ef440308642cc8d020c7a18539e46444152b2d
caren-achieng <caren.achieng@strathmore.edu>
2022-10-13 11:55:45 +0300

    Personality

--- app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
+++ app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
@@ -1,16 +1,25 @@
 package com.lc.roomie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
 import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_complete_profile.*
+import kotlinx.android.synthetic.main.activity_houserules.*
 
 
 class CompleteProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_complete_profile)
+
+        continueProfile.setOnClickListener{
+            val intent = Intent(this, PersonalityActivity::class.java)
+            startActivity(intent)
+        }
+
         var datepicker = findViewById<DatePicker>(R.id.birthday)
         datepicker.calendarViewShown = false
     }
