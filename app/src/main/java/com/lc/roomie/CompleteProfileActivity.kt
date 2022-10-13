5fb79ba0242717daf46ecff4244671f88082effb
Leo-Bikuri <leo.bikuri@strathmore.edu>
2022-10-13 20:03:01 +0300

    Profile

--- app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
+++ app/src/main/java/com/lc/roomie/CompleteProfileActivity.kt
@@ -1,11 +1,15 @@
 package com.lc.roomie
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.MmsSms.PendingMessages
 import android.view.View
-import android.widget.DatePicker
-import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_complete_profile.*
 
 
 class CompleteProfileActivity : AppCompatActivity() {
@@ -14,11 +18,32 @@ class CompleteProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_complete_profile)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        var datepicker = findViewById<DatePicker>(R.id.birthday)
-        datepicker.calendarViewShown = false
+        birthday.calendarViewShown = false
+
+        change_profile_picture.setOnClickListener {
+            if (VERSION.SDK_INT >= VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                    var permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+                    requestPermissions(permissions, PERMISSION_CODE)
+
+                }else{
+
+                }
+            }else{
+
+            }
+        }
+
     }
 
     fun onRadioButtonClicked(view: View) {}
+    companion object{
+        private val IMAGE_PICK_CODE = 1000;
+
+        private val PERMISSION_CODE = 1001
+    }
 }
 
 //fun onRadioButtonClicked(view: View) {
